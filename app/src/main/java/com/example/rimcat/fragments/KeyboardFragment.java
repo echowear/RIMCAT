3e92795dca142cab1ee820652910cf568d96924e
Andrew Peltier <andrewcpeltier@gmail.com>
2020-10-04 13:52:05 -0400

    Changes made to keyboard screen

--- app/src/main/java/com/example/rimcat/fragments/KeyboardFragment.java
+++ app/src/main/java/com/example/rimcat/fragments/KeyboardFragment.java
@@ -3,12 +3,18 @@ package com.example.rimcat.fragments;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.speech.RecognizerIntent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,26 +32,32 @@ public class KeyboardFragment extends QuestionFragment {
 
     private static final String     TAG = "RecallResponseFragment";
     private Context mContext;
+    private Vibrator mVibrator;
+    private TextView promptText;
     private EditText responseText;
     private Button submitBtn;
     private FloatingActionButton audioBtn;
+    int currentScreen = 0;
+    boolean wasMicPressed;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_keyboard, container, false);
         mContext = view.getContext();
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
         responseText = view.findViewById(R.id.keyboard_recall_word);
         audioBtn = view.findViewById(R.id.keyboard_audio_btn);
         submitBtn = view.findViewById(R.id.keyboard_addBtn);
+        promptText = view.findViewById(R.id.keyboard_text);
 
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (responseText.getText().toString() != null && responseText.getText().toString().toLowerCase().equals("hello"))
-                    ((MainActivity)getActivity()).getFragmentData(null);
+                    moveToNextTask();
                 else {
-                    Toast t = Toast.makeText(mContext, "Make sure that the text box has the word 'Hello' in it.", Toast.LENGTH_LONG);
+                    Toast t = Toast.makeText(mContext, "Make sure that the text box has the word 'hello' in it.", Toast.LENGTH_LONG);
                     ViewGroup group = (ViewGroup) t.getView();
                     TextView toastTV = (TextView) group.getChildAt(0);
                     toastTV.setTextSize(20);
@@ -59,6 +71,7 @@ public class KeyboardFragment extends QuestionFragment {
         audioBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                wasMicPressed = true;
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
 
@@ -76,12 +89,59 @@ public class KeyboardFragment extends QuestionFragment {
                 }
             }
         });
+        audioBtn.hide();
 
         cardView = view.findViewById(R.id.card);
         startAnimation(true);
         logStartTime();
         nextButtonReady();
         return view;
+    }
+
+    private void moveToNextTask() {
+        if (currentScreen == 0) {
+            currentScreen++;
+            promptText.setText(R.string.keyboard_prompt2);
+            ((MainActivity)getActivity()).hideSoftKeyboard();
+            responseText.setText("");
+            audioBtn.show();
+            vibrateToastAndExecuteSound("hello");
+        } else if (wasMicPressed) {
+            ((MainActivity)getActivity()).getFragmentData(null);
+        } else {
+            Toast t = Toast.makeText(mContext, "Try using the microphone button at least once.", Toast.LENGTH_LONG);
+            ViewGroup group = (ViewGroup) t.getView();
+            TextView toastTV = (TextView) group.getChildAt(0);
+            toastTV.setTextSize(20);
+            t.show();
+        }
+    }
+
+    private void vibrateToastAndExecuteSound(String submitText) {
+        // Vibrate the device
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && mVibrator != null) {
+            mVibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else if (mVibrator != null) {
+            //deprecated in API 26
+            mVibrator.vibrate(500);
+        }
+
+        // Toast affirmative message
+        Toast t = Toast.makeText(getActivity(), "'" + submitText + "' submitted! Keep going!", Toast.LENGTH_LONG);
+        ViewGroup group = (ViewGroup) t.getView();
+        TextView toastTV = (TextView) group.getChildAt(0);
+        toastTV.setTextSize(20);
+//        t.setGravity(Gravity.TOP, 0, 5);
+        t.show();
+
+        // Execute sound
+        try {
+            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone(mContext, notification);
+            r.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void setResponseTextToSpeechText(String speechText) {
